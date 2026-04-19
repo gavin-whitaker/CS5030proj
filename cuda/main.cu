ecdc630cf0af736476f6ebe970dedae922f07b15
Curt Reyes <curtreyes00@yahoo.com>
2026-04-18 21:00:21 -0600

    rewrite, so I understand it

--- cuda/main.cu
+++ cuda/main.cu
@@ -2,17 +2,13 @@
 
 #include "utils/args.h"
 
-#include <cstdlib>
-#include <iostream>
-
 int main(int argc, char **argv) {
-  Config cfg;
-  cfg.output = "results/cuda_out.csv";
-  parse_args(argc, argv, cfg);
-  if (cfg.input.empty()) {
-    print_usage(argv[0], "cuda --block_size <int>");
-    return 1;
-  }
-  return run_kmeans_cuda(cfg);
+	Config cfg;
+	cfg.output = "results/cuda_out.csv";
+	parse_args(argc, argv, cfg);
+	if (cfg.input.empty()) {
+		print_usage(argv[0], "cuda --block_size <int>");
+		return 1;
+	}
+	return run_kmeans_cuda(cfg);
 }
-
--- openmp/main.cpp
+++ openmp/main.cpp
@@ -2,17 +2,13 @@
 
 #include "utils/args.h"
 
-#include <cstdlib>
-#include <iostream>
-
 int main(int argc, char **argv) {
-  Config cfg;
-  cfg.output = "results/openmp_out.csv";
-  parse_args(argc, argv, cfg);
-  if (cfg.input.empty()) {
-    print_usage(argv[0], "openmp [--threads <int>]");
-    return 1;
-  }
-  return run_kmeans_openmp(cfg);
+	Config cfg;
+	cfg.output = "results/openmp_out.csv";
+	parse_args(argc, argv, cfg);
+	if (cfg.input.empty()) {
+		print_usage(argv[0], "openmp [--threads <int>]");
+		return 1;
+	}
+	return run_kmeans_openmp(cfg);
 }
-
